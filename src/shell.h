ceefea7fb30d381ef8a7880bcdcbee20062e70e4
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-06-29 11:46:36 +0800

    feat(shell): write documentation

--- src/shell.h
+++ src/shell.h
@@ -1,49 +1,85 @@
+/**
+ * @file shell.h
+ *
+ * Declarations for the shell context and associated functions.
+ */
+
 #ifndef SHELL_H
 #define SHELL_H
 
 #include <stdbool.h>
 #include <stdlib.h>
 
+/** Keeps track of various stateful information about the current shell. */
 struct sh_shell_context {
-    size_t history_capacity;
-    size_t history_count;
-    char **history;
+    size_t history_capacity; /**< Capacity of the history array. */
+    size_t history_count;    /**< Current number of entries in the history. */
+    char **history;          /**< Array of command history strings. */
 
     char *prompt; /**< The current shell prompt. */
 
-    bool should_exit;
+    bool should_exit; /**< Indicates if the shell should exit. This is set by
+                         the `exit` builtin. */
     union {
+        /**
+         * Exit code for the shell if the shell should exit..
+         * This is set by the `exit` builtin and is only initialised if the
+         * shell should exit.
+         */
         int exit_code;
     };
 };
 
+/** Represents the possible results of initializing the shell context. */
 enum sh_init_shell_context_result {
-    SH_INIT_SHELL_CONTEXT_SUCCESS,
-    SH_INIT_SHELL_CONTEXT_MEMORY_ERROR,
+    SH_INIT_SHELL_CONTEXT_SUCCESS,     /**< Successful initialization. */
+    SH_INIT_SHELL_CONTEXT_MEMORY_ERROR /**< Memory allocation error. */
 };
 
+/**
+ * Initializes the shell context.
+ *
+ * @param ctx a pointer to the shell context to initialise
+ * @return the result of the initialization
+ */
 enum sh_init_shell_context_result
 init_shell_context(struct sh_shell_context *ctx);
 
+/**
+ * Represents the possible results for adding a line to the shell's
+ * command history.
+ */
 enum sh_add_to_history_result {
-    SH_ADD_TO_HISTORY_SUCCESS,
-    SH_ADD_TO_HISTORY_MEMORY_ERROR,
+    SH_ADD_TO_HISTORY_SUCCESS,     /**< Successful addition to history. */
+    SH_ADD_TO_HISTORY_MEMORY_ERROR /**< Memory allocation error. */
 };
 
+/**
+ * Adds a line to the shell history.
+ *
+ * @param ctx a pointer to the shell context
+ * @param line the command line to be added to history
+ * @return the result of the addition to history
+ */
 enum sh_add_to_history_result
 add_to_history(struct sh_shell_context *ctx, char *line);
 
+/**
+ * Destroys the shell context by freeing allocated resources.
+ *
+ * @param ctx a pointer to the shell context
+ */
 void destroy_shell_context(struct sh_shell_context *ctx);
 
-/** Sets up signal handling. */
+/** Sets up signal handling for the shell. */
 void setup_signals();
 
 /**
- * Handler for `SIGCHLD` signal.
+ * Handler for the `SIGCHLD` signal.
  *
  * This handler consumes background processes with `waitpid()` to make sure they
  * do not become zombie processes.
  */
 void handle_sigchld(int signo);
 
-#endif
+#endif /* SHELL_H */
