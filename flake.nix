fef7beca1b4293529909255ab622174765c8d23e
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-15 16:48:18 +0800

    feat(flake, build): build with nix

--- flake.nix
+++ flake.nix
@@ -5,11 +5,33 @@
   };
 
   outputs =
-    { flake-utils, nixpkgs, ... }:
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        acush = pkgs.stdenv.mkDerivation {
+          pname = "acush";
+          version = "1.0.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            gnumake
+            gcc
+          ];
+
+          installPhase = ''
+            mkdir -p "$out/bin"
+            cp "build/acush" "$out/bin"
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -24,6 +46,23 @@
             typst
           ];
         };
+
+        packages = {
+          inherit acush;
+          default = acush;
+        };
+
+        apps =
+          let
+            acushApp = {
+              type = "app";
+              program = "${self.packages.${system}.default}/bin/acush";
+            };
+          in
+          {
+            acush = acushApp;
+            default = acushApp;
+          };
       }
     );
 }
